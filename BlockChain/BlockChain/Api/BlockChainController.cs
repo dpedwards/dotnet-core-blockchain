bb16c48114dcc66d70a010b2e1505a226643e7c2
Davain Pablo Edwards <core8@gmx.net>
2019-02-27 09:59:42 +0100

    :star: add blockchain controller class for executing actions

---
+++ BlockChain/BlockChain/Api/BlockChainController.cs
@@ -0,0 +1,27 @@
+﻿using BlockChain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlockChain.Api
+{
+
+   /**
+    * Used to define blockchain interactions
+    * 
+    * @author Davain Pablo Edwards
+    * @license MIT 
+    * @version 1.0
+    */
+    [Produces("application/json")]
+    [Route("")]
+    public class BlockChainController : Controller
+    {
+
+        // Initial cryptocurrency mechanisms
+        public static CryptoCurrency blockchain = new CryptoCurrency();
+
+       
+
+       
+
+    }
+}
--- BlockChain/BlockChain/Models/CryptoCurrency.cs
+++ BlockChain/BlockChain/Models/CryptoCurrency.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Net;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BlockChain.Models
 {
